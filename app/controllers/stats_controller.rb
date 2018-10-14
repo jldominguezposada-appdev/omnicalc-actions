31b7f8b98f080ef367402604f1e8f42a6dcf20a7
Jose Luis Dominguez de Posada <jdoming1@chicagobooth.edu>
2018-10-14 19:46:54 +0000

    Clase_Week4_2

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,36 +7,125 @@ class StatsController < ApplicationController
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer"
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer"
+    @count = @numbers.count
+    
+     minimum = [@numbers.at(0)]
+  
+          @numbers.each do |subject|
+      
+                if subject < minimum.at(0)
+                  
+                  minimum.delete_at(0)
+                  minimum.push(subject)
+        
+                end
+          end
 
-    @minimum = "Replace this string with your answer"
+    @minimum = minimum.at(0)
+  
+       maximum = [@numbers.at(0)]
+  
+          @numbers.each do |subject|
+      
+                if subject > maximum.at(0)
+                  
+                  maximum.delete_at(0)
+                  maximum.push(subject)
+        
+                end
+          end
 
-    @maximum = "Replace this string with your answer"
+    @maximum = maximum.at(0)
 
-    @range = "Replace this string with your answer"
+    @range = maximum.at(0) - minimum.at(0)
 
     # Median
+    
+    total_elements = @numbers.count
+    
+    even_test = total_elements / 2
+      
+        if total_elements.even?
+          
+          median = (@sorted_numbers.at(even_test) + @sorted_numbers.at(even_test - 1)) / 2
+          
+        else
+          
+          median = @sorted_numbers.at(even_test.round(0)-1)
+          
+        end
+    
     # ======
 
-    @median = "Replace this string with your answer"
+    @median = median
 
-    @sum = "Replace this string with your answer"
+      sum = [0]
+    
+        @numbers.each do |subject|
+    
+          sum_step = sum.at(0) + subject
+          
+          sum.delete_at(0)
+          sum.push(sum_step)
+        
+        end
+        
+    @sum = sum.at(0)
 
-    @mean = "Replace this string with your answer"
+        mean = sum.at(0) / total_elements
+
+    @mean = mean
 
     # Variance
+    
+      sum_squares_difference = [0]
+      
+        @numbers.each do |subject_b|
+          
+          difference_mean = subject_b - mean
+          square_difference = difference_mean**2
+          sum_squares_difference_step = sum_squares_difference.at(0) + square_difference
+          
+          sum_squares_difference.delete_at(0)
+          sum_squares_difference.push(sum_squares_difference_step)
+          
+      end
+  
+    variance = sum_squares_difference.at(0) / total_elements
+    
+    
     # ========
+  
 
-    @variance = "Replace this string with your answer"
-
-    @standard_deviation = "Replace this string with your answer"
+    @variance = variance
+    
+    @standard_deviation = variance**0.5
 
     # Mode
+    
+    mode_hash = Hash.new(0)
+  
+        @sorted_numbers.each do |subject_c|
+          
+            mode_hash[subject_c] +=1
+            
+        end
+      
+    mode_array = []
+
+      mode_hash.each do |key,value|
+      
+        if value == mode_hash.values.max
+            mode_array.push(key)
+        end
+        
+    end
+    
     # ====
 
-    @mode = "Replace this string with your answer"
+    @mode = mode_array.sort.join(", ")
 
     # ================================================================================
     # Your code goes above.
